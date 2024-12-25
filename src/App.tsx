59a1c211127ae125796822f2523c2e4c7d58f4b2
Rashidprojects <rashidgitproject@gmail.com>
2024-12-25 13:54:15 +0530

    klimate project initialized

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter } from "react-router-dom"
+import Layout from "./components/Layout"
+import { ThemeProvider } from "./context/theme-provider"
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark">
+        <Layout>Hello</Layout>
+      </ThemeProvider>
+    </BrowserRouter>
+  )
+} 
+ 
+export default App
