c0602fe9e9eb938e83b7a4a66385fd6e317b863a
Rashidprojects <rashidgitproject@gmail.com>
2024-12-26 20:42:08 +0530

    current weather component completed

--- src/components/loading-skeleton.tsx
+++ src/components/loading-skeleton.tsx
@@ -2,14 +2,14 @@ import { Skeleton } from "./ui/skeleton"
 
 const WeatherSkeleton = () => {
   return (
-    <div className="space-y-6">
+    <div className="space-y-6 gap-3">
         <div className="grid gap6">
-            <Skeleton className="h-[300px] w-full rounded-lg " />
-            <Skeleton className="h-[300px] w-full rounded-lg " />
-            <div className="grid gap-6 md:grid-cols-2">
-                <Skeleton className="h-[300px] w-full rounded-lg " />
-                <Skeleton className="h-[300px] w-full rounded-lg " />
-            </div>
+          <Skeleton className="h-[300px] w-full rounded-lg mb-6  " />
+          <Skeleton className="h-[300px] w-full rounded-lg " />
+        </div>
+        <div className="grid gap-6 md:grid-cols-2">
+          <Skeleton className="h-[300px] w-full rounded-lg " />
+          <Skeleton className="h-[300px] w-full rounded-lg " />
         </div>
     </div>
   )
