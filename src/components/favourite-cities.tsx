1d335a02873889d9a8807a1291ffa2adf0c8223b
Rashidprojects <166205136+Rashidprojects@users.noreply.github.com>
2025-01-01 22:43:50 +0530

    Update favourite-cities.tsx

--- src/components/favourite-cities.tsx
+++ src/components/favourite-cities.tsx
@@ -34,6 +34,7 @@ const FavouriteCities = () => {
                         />
                 })}
             </div>
+          <ScrollBar orientation="horizontal" />
         </ScrollArea>
     </>
   );
